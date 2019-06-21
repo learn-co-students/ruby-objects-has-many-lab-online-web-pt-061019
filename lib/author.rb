f8d10ab356d74b5ea9e05c396d84ffd5a4158037
Huy Do <huyddo40@gmail.com>
2019-06-21 02:06:58 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self #=> author
+  end
+
+  def add_post_by_title(title)
+    post = Author.new(title)
+    post.author = self
+  end
+
+  def title
+
+  end
+end
