a4793cf649895ed3942b4599cc492c3c4c08d23f
Chris Junker <elevatemedfit@gmail.com>
2019-07-16 01:52:08 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+  attr_accessor :artist, :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+def artist_name
+  if artist
+ self.artist.name
+ else
+  nil
+end
+end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+
+
+
+
+
+
+end
